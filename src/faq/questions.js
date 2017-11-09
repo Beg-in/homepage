46680479c2275e5f7cfd4b485531f42add110d00
Brandon Frisch <bfrisch1@gmail.com>
2017-11-09 04:10:19 +0000

    chore(faq): started working on a few questions

--- src/faq/questions.js
+++ src/faq/questions.js
@@ -1,12 +1,12 @@
 module.exports = {
-  'Getting Started': {
-    'Question 1': 'Answer answer answer',
-    'Question 2': 'Answer answer answer',
+  'General': {
+    "What do I say to my investors if they don't want me to outsource my company's software development?": "There are a number of good reasons to outsource software development when you're a small startup. Here are a few:Your core competency, which you're amazing at, is not software development. You're planning on hiring developers but want to get a head start on the prototype first. You're using the prototype to convince developers to join your startup. You want to cheaply validate a concept before you make a larger investment. The company you're using to outsource has a fantastic track record and is going to do a great job with your product. It's cheaper to outsource than to have your in-house developers build your product.",
+    'How long will my project take?': 'Depending on complexity, most projects fall within the four to ten month range.',
     'Question 3': 'Answer answer answer',
   },
   'Project Quotes': {
-    'How are you so cheap?': 'Answer answer answer',
-    'How are you so expensive?': 'Answer answer answer',
+    'How much will my project cost?': 'Sometimes developer marketplaces can beat our pricing with less experienced talent. We’ve seen from personal experience & heard from many of our customers that these projects often go poorly. As is the case with most major buying decisions, you get what you pay for.',
+    'Are you guys open to partnering for equity':'At this stage, we not taking on equity projects',
     'Question 3': 'Answer answer answer',
   },
   'Example section 3': {
