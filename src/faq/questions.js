49c8125d1a1fb6c20f0ad924d16262c5a733ae00
Rich <rkim43@uic.edu>
2017-11-09 04:53:52 +0000

    chore(faq): add questions and answers

--- src/faq/questions.js
+++ src/faq/questions.js
@@ -2,16 +2,16 @@ module.exports = {
   'General': {
     "What do I say to my investors if they don't want me to outsource my company's software development?": "There are a number of good reasons to outsource software development when you're a small startup. Here are a few:Your core competency, which you're amazing at, is not software development. You're planning on hiring developers but want to get a head start on the prototype first. You're using the prototype to convince developers to join your startup. You want to cheaply validate a concept before you make a larger investment. The company you're using to outsource has a fantastic track record and is going to do a great job with your product. It's cheaper to outsource than to have your in-house developers build your product.",
     'How long will my project take?': 'Depending on complexity, most projects fall within the four to ten month range.',
-    'Question 3': 'Answer answer answer',
+    'Is the development team internal?': 'Yes, we have developers located in both Chicago and Bloomington, IL.',
   },
   'Pricing': {
     'How much will my project cost?': 'Sometimes developer marketplaces can beat our pricing with less experienced talent. We’ve seen from personal experience & heard from many of our customers that these projects often go poorly. As is the case with most major buying decisions, you get what you pay for.',
-    'Are you guys open to partnering for equity?':'At this stage, we not taking on equity projects',
+    'Are you guys open to partnering for equity?':'At this stage, we are not taking on equity projects',
     'Question 3': 'Answer answer answer',
   },
-  'Example section 3': {
-    'Question 1': 'Answer answer answer',
-    'Question 2': 'Answer answer answer',
-    'Question 3': 'Answer answer answer',
+  'Development': {
+    'How would you divide your team up and prioritize the work for our project?': 'Depending on the time constraint for the project, we would split up the developers accordingly.',
+    'How does the scaling work for your servers/databases?': 'We have a robust AWS system running that would automatically adjust to traffic by scaling horizontally; this would essentially mean to add or remove servers based on what is needed.',
+    'Do you guys focus on mobile applications?': 'We create hybrid mobile applications which is a very economical way of making iOS and Android apps. Any changes reflected on the web application would also be applied to the mobile applications, thus allowing updating and maintaining the application to be a breeze. ',
   },
 };
