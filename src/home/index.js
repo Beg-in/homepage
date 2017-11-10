b30aaf8b1b14201b787dbea7669bd0d7a099a562
Brandon Frisch <bfrisch1@gmail.com>
2017-11-10 03:38:18 +0000

    chore(home): added slate section of home page

--- src/home/index.js
+++ src/home/index.js
@@ -3,7 +3,13 @@ module.exports = {
     cube: require('./images/cube.svg'),
     stats: require('./images/stats.svg'),
     slate: require('./images/slate.svg'),
-
+    gears: require('./images/gears.svg'),
+    orangegear: require('./images/gear-orange.svg'),
+    scale: require('./images/scale.svg'),
+    vault: require('./images/vault.svg'),
+    multi: require('./images/multi.svg'),
+    lock: require('./images/lock.svg'),
+    crane: require('./images/crane.svg'),
   }),
 };
 
