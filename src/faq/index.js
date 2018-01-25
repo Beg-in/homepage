d5a5e85dc986a2799f4f8a5342e8e34af804df06
Cody Sherman <meatwaddancin@gmail.com>
2018-01-25 03:39:15 +0000

    feat(faq): add scrolling to section

--- src/faq/index.js
+++ src/faq/index.js
@@ -1,8 +1,17 @@
 let questions = require('./questions');
 
+const MAIN = window;
+
+let delay = (duration = 100) => new Promise(resolve => setTimeout(() => resolve(), duration));
+
 module.exports = {
   data: () => ({
     questions,
   }),
-};
 
+  methods: {
+    async doScroll(ref) {
+     this.$refs[ref][0].scrollIntoView({ behavior: 'smooth' });
+    },
+  },
+};
