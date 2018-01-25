6f15e778637a5c1184acae84a47ee0ba8d49c523
Brandon Frisch <bfrisch1@gmail.com>
2018-01-25 20:59:37 +0000

    chore(team): updated format, styles, and bios

--- src/data/team/index.js
+++ src/data/team/index.js
@@ -1,46 +1,46 @@
 module.exports = {
-    team: [
-        {
-            firstName: 'Johnmark',
-            lastName: 'Beaty',
-            img: require('./images/johnmark.png'),
-            title: 'Vue Master',
-            description: 'test bio',
-        },
-        {
-            firstName: 'Brandon',
-            lastName: 'Frisch',
-            img: require('./images/brandon.png'),
-            title: 'Managing Director',
-            description: 'test bio',
-        },
-        {
-            firstName: 'Cody',
-            lastName: 'Sherman',
-            img: require('./images/cody.png'),
-            title: 'Float En Fuego',
-            description: 'test bio',
-        },
-        {
-            firstName: 'Rich',
-            lastName: 'Kim',
-            img: require('./images/rich.png'),
-            title: 'President',
-            description: 'test bio',
-        },
-        {
-            firstName: 'Brian',
-            lastName: 'Jesse',
-            img: require('./images/brian.png'),
-            title: 'Senior Chewbacca',
-            description: 'test bio',
-        },
-        {
-            firstName: 'Tony',
-            lastName: 'Dykstra',
-            img: require('./images/tony.png'),
-            title: 'VP of VP of Physical Marketing',
-            description: 'test bio',
-        },
-    ]
+  team: [
+    {
+      firstName: 'Brandon',
+      lastName: 'Frisch',
+      img: require('./images/brandon.png'),
+      title: 'Managing Director',
+      description: 'Brandon is the co-founder and managing director of Beg.in. He works to intersect great designers, technologists, and strategists on the most interesting problems - then somehow manages to get out of their way.',
+    },
+    {
+      firstName: 'Cody',
+      lastName: 'Sherman',
+      img: require('./images/cody.png'),
+      title: 'Lead Front-End',
+      description: 'Cody Sherman is the lead front-end for Beg.in\'s projects, specializing in clean and modern UI and UX. Cody is a self declared CSS wizard, tech enthusiast, and video game addict. He has been lead front-end developer for multiple start-ups, and runs the open source CSS framework, Frow CSS, in his free time. Cody last looked up from his computer screen in 2013, but has high hopes to change that in the upcoming year.',
+    },
+    {
+      firstName: 'Brian',
+      lastName: 'Jesse',
+      img: require('./images/brian.png'),
+      title: 'Lead Back-end',
+      description: 'Brian Jesse is a co-founder of Beg.in, lead back-end developer, and application architect. He is an experienced CTO, a career entrepreneur, and a full-stack software developer with skills ranging from database management to 3d graphics and artificial intelligence. If Brian isn\'t playing video games he\'s developing his own, or when he (rarely) gets outside he\'s probably skiing.',
+    },
+    {
+      firstName: 'Johnmark',
+      lastName: 'Beaty',
+      img: require('./images/johnmark.png'),
+      title: 'Software Engineer',
+      description: 'Johnmark is a back-end developer for Beg.in. If he’s not working, you can find him spending time longboarding, sending snaps or involved in activities with student campus ministries.',
+    },
+    {
+      firstName: 'Rich',
+      lastName: 'Kim',
+      img: require('./images/rich.png'),
+      title: 'President',
+      description: 'test bio',
+    },
+    {
+      firstName: 'Tony',
+      lastName: 'Dykstra',
+      img: require('./images/tony.png'),
+      title: 'Dir. of Business Development',
+      description: 'Tony serves as Director of Business Development for Beg.in. He thrives on understanding ideas and building real relationships. He does not thrive on gas station sushi or tequila, but who does?',
+    },
+  ]
 };
