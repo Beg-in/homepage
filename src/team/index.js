dbf3aafe25ab8529b701aefe4ca1b2477f4fbc5e
Cody Sherman <meatwaddancin@gmail.com>
2018-01-27 02:58:11 +0000

    refactor(contact-footer): create the contact footer component

--- src/team/index.js
+++ src/team/index.js
@@ -2,6 +2,7 @@ let { team } = require('../data/team');
 
 module.exports = {
   components: {
+    contactFooter: require('../contact-footer/vue.pug'),
     pictureSvg: require('../home/images/cube.svg'),
   },
 
