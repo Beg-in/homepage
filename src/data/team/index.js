f3a20dec2b8e0c5f553a8ae8fe3870acf4020f23
Brandon Frisch <bfrisch1@gmail.com>
2019-05-08 22:20:42 +0000

    chore(home faq team): updated site after brian left

--- src/data/team/index.js
+++ src/data/team/index.js
@@ -15,17 +15,10 @@ module.exports = {
       description: 'Cody specializes in clean and modern UI and UX. He is a self declared CSS wizard, tech enthusiast, and video game addict. He has worked for multiple start-ups and runs the open source Frow CSS in his free time. Cody last looked up from his computer screen in 2013, but has high hopes to change that in the upcoming year.',
     },
     {
-      firstName: 'Brian',
-      lastName: 'Jesse',
-      img: require('./images/brian.png'),
-      title: 'Lead Back-end',
-      description: 'Brian is an experienced CTO, career entrepreneur, and a full-stack software developer with skills ranging from database management to 3D graphics and artificial intelligence. If he isn\'t playing video games he\'s developing his own, or when he (rarely) gets outside he\'s probably skiing.',
-    },
-    {
       firstName: 'Johnmark',
       lastName: 'Beaty',
       img: require('./images/johnmark.png'),
-      title: 'Software Engineer',
+      title: 'Lead Software Engineer',
       description: 'Johnmark is a back-end developer for Beg.in. If he’s not working, you can find him spending time longboarding, sending snaps or involved in activities with student campus ministries.',
     },
     {
@@ -35,12 +28,12 @@ module.exports = {
       title: 'Full-Stack Developer',
       description: 'Richard is a full-stack developer for Beg.in. When he is not working, he likes to exercise, create jazz music, and stream video games professionally. He also likes talking about sports and cooking.',
     },
-    {
-      firstName: 'Tony',
-      lastName: 'Dykstra',
-      img: require('./images/tony.png'),
-      title: 'Dir. of Business Development',
-      description: 'Tony serves as Director of Business Development for Beg.in. He thrives on understanding ideas and building real relationships. He does not thrive on gas station sushi or tequila, but who does?',
-    },
+    // {
+    //   firstName: 'Tony',
+    //   lastName: 'Dykstra',
+    //   img: require('./images/tony.png'),
+    //   title: 'Dir. of Business Development',
+    //   description: 'Tony serves as Director of Business Development for Beg.in. He thrives on understanding ideas and building real relationships. He does not thrive on gas station sushi or tequila, but who does?',
+    // },
   ]
 };
