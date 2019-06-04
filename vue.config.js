3407270f1bbd567969bd2e92f4b0050bf1691807
Johnmark Beaty <johnmarkbeaty@gmail.com>
2019-06-04 08:24:02 -0500

    fix svg loader

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,21 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/homepage/' : '/',
-  chainWebpack: config => {
-    const svgRule = config.module.rule('svg')
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule('svg');
 
     // clear all existing loaders.
     // if you don't do this, the loader below will be appended to
     // existing loaders of the rule.
-    svgRule.uses.clear()
+    svgRule.uses.clear();
 
     // add replacement loader(s)
     svgRule
       .use('vue-svg-loader')
-        .loader('vue-svg-loader')
+      .loader('vue-svg-loader')
+      .options({
+        svgo: {
+          plugins: [{ removeDimensions: true }, { removeViewBox: false }],
+        },
+      });
   },
 };
