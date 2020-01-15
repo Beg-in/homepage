c00483c1431ab3b62c0fa25a0c81338b3ce2ef2a
Cody Sherman <meatwaddancin@gmail.com>
2020-01-15 16:05:03 -0600

    remove hashtag from urls

--- src/main.js
+++ src/main.js
@@ -8,4 +8,11 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   render: (h) => h(App),
+  created() {
+    if (sessionStorage.redirect) {
+      const redirect = sessionStorage.redirect;
+      delete sessionStorage.redirect;
+      this.$router.push(redirect);
+    }
+  },
 }).$mount('#app');
